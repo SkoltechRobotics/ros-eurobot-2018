f1977b36f0074ba1b0b4fd101004335c448fb0d2
safoex <safoex@hotmail.com>
2017-12-21 10:19:41 +0300

    tree_description

---
+++ eurobot_decision_maker/src/python/tree_description.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+import re
+
+pub = None
+
+
+if __name__  == '__main__':
+    rospy.init_node('fake_stm', anonymous =True)
+    
+    pub = rospy.Publisher('bt_maker', String, queue_size = 10)
+    #sub = rospy.Subscriber('robot_command', String, fake_response)
+    data = ""
+    with open("tree_description.txt") as btf:
+        data = btf.read().replace('\n',' ')
+    rospy.loginfo(data)
+    pub.publish(data)
+    rospy.spin()
