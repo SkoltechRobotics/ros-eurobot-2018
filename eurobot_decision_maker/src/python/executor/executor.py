bd6fe91e5a71c8e135a0da1a3fbd12e9f5d7547a
safoex <safoex@hotmail.com>
2018-01-15 02:49:14 +0300

    executor: started ActionNode class

--- eurobot_decision_maker/src/python/executor/executor.py
+++ eurobot_decision_maker/src/python/executor/executor.py
@@ -8,6 +8,8 @@ import re
 pub = None
 
 
+	
+
 class TickEngine:
 	def __init__(self, initial_state):
 		self.state = initial_state
@@ -21,9 +23,32 @@ class TickEngine:
 
 class TreeNode:
 	def __init__(self, parent):
-		self.status = False 	# True if active right now
+		self.status = "not started"
 		self.parent = parent
 	
+
+class ActionNode(TreeNode):
+	def __init__(self,command_id, command_topic, message, request_topic):
+		self.id = command_id
+		self.command_pub = command_topic 	# this is ros publisher
+		self.request_topic_name = request_topic # this is name, subscriber will be created later
+		self.status = "not started"
+		self.message = message
+	def callback_for_terminating(self):
+		def cb(msg):
+			action_id, action_status = re.match("(\S*)\w(\S*)",msg.data).group(??) # finish it!
+			if action_id == self.command_id:
+				self.status = action_status
+		return cb
+	def start():
+		self.status = "active"
+		self.command_pub.publish(self.command_id + ' ' + self.message)
+		self.sub = rospy.Subscriber(self.request_topic_name, String, self.callback_for_terminating())
+	
+	def finish():
+		self.status = "finished"
+		self.sub.unregister()
+		
 		
 class ControlNode(TreeNode):
 	def __init__(self, parent):
