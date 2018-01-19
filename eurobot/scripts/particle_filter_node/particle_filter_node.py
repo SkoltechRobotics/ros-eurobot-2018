2337a6027868083678d592fd53b0ee2e8181c35d
Viktor Liviniuk <viktor.liviniuk@gmail.com>
2018-01-19 15:23:08 +0300

    fixed particles viz

--- eurobot/scripts/particle_filter_node/particle_filter_node.py
+++ eurobot/scripts/particle_filter_node/particle_filter_node.py
@@ -29,7 +29,7 @@ def stm_coordinates_callback(data):
     pub.publish(' '.join(map(str, coords)))
 
     # create and pub PointArray with particles    
-    poses = [Pose(Point(x=particle_filter.particles[i,0]/1000, y=particle_filter.particles[i,1]/1000, z=0), tf.transformations.quaternion_from_euler(0, 0, particle_filter.particles[i,2])) for i in range(len(particle_filter.particles))]
+    poses = [Pose(Point(x=particle_filter.particles[i,0]/1000, y=particle_filter.particles[i,1]/1000, z=.4), Quaternion(*quaternion_from_euler(0, 0, particle_filter.particles[i,2]))) for i in range(len(particle_filter.particles))]
     header = Header(frame_id="world")
     particles = PoseArray(header=header, poses=poses)
     pub_particles.publish(particles)
