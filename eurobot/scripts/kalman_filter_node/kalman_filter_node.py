a85a9ba1d4622b77ff75af56873adb232e6a3a23
Viktor Liviniuk <viktor.liviniuk@gmail.com>
2018-01-30 11:09:07 +0300

    added angle to kalman filter

--- eurobot/scripts/kalman_filter_node/kalman_filter_node.py
+++ eurobot/scripts/kalman_filter_node/kalman_filter_node.py
@@ -5,21 +5,23 @@ import numpy as np
 import time
 
 class kalman_filter():
-    def __init__(self, initial_coords = np.zeros(3), sigma_a = 1, sigma_eta = 0.02):
+    def __init__(self, initial_coords = np.zeros(3), sigma_acc = 10, sigma_measure_x = 0.05, sigma_measure_a = 0.01):
         self.speed = np.matrix([0, 0]).T
 
-        self.sigma_a = sigma_a
-        self.sigma_eta = sigma_eta
-        
-        self.R = np.matrix(np.eye(2)*self.sigma_eta**2)
-        self.H = np.matrix([[1,0,0,0],[0,0,1,0]])
-        self.Ht = self.H.T
-        self.I = np.matrix(np.eye(4))
+        self.sigma_acc = sigma_acc
+        self.sigma_measure_x = sigma_measure_x
+        self.sigma_measure_a = sigma_measure_a
 
-        self.Xp = np.matrix(np.zeros((4,1)))
+        self.R = np.matrix(np.diag((self.sigma_measure_x**2, self.sigma_measure_x**2, self.sigma_measure_a**2)))
+        self.H = np.matrix([[1,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0]])
+        self.Ht = self.H.T
+        self.I = np.matrix(np.eye(6))
+
+        self.Xp = np.matrix(np.zeros((6,1)))
         self.Xp[0] = initial_coords[0]
         self.Xp[2] = initial_coords[1]
-        self.Pp = np.matrix(np.eye(4)*1e4)
+        self.Xp[4] = initial_coords[2]
+        self.Pp = np.matrix(np.eye(6)*1e4)
 
         # initial iteration to avoid checking on it
         self.t_prev = time.clock()
@@ -32,15 +34,16 @@ class kalman_filter():
         now = time.clock()
         T = now - self.t_prev
         self.t_prev = now
-        self.F = np.matrix([[1, T, 0, 0], [0, 1, 0, 0], [0, 0, 1, T], [0, 0, 0, 1]])
+        self.F = np.matrix([[1, T, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, T, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, T], [0, 0, 0, 0, 0, 1]])
         self.Ft = self.F.T
-        self.G = np.matrix([[T**2/2, 0], [T, 0], [0, T**2/2], [0, T]])
+        self.G = np.matrix([[T**2/2, 0, 0], [T, 0, 0], [0, T**2/2, 0], [0, T, 0], [0, 0, T**2/2], [0, 0, T]])
         self.Gt = self.G.T
-        self.Q = self.G * self.Gt * self.sigma_a**2
+        self.Q = self.G * self.Gt * self.sigma_acc**2
 
     def predict(self):
         #self.Xf[1] = self.speed[0]
         #self.Xf[3] = self.speed[1]
+        #self.Xf[5] = self.speed[2]
         self.Xp = self.F * self.Xf
         self.Pp = self.F * self.Pf * self.Ft + self.Q
 
@@ -50,19 +53,21 @@ class kalman_filter():
         self.Pf = (self.I - self.K * self.H) * self.Pp
 
     def iterate(self, z):
-        Zm = np.matrix(z[:2]).T
+        t1 = time.clock()
+        Zm = np.matrix(z).T
         self.reset_T()
         self.predict()
         self.update(Zm)
+        print 'kalman iteration took', time.clock() - t1, 'sec'
         return self.Xf
 
     def set_speed(self, v):
-        self.speed = np.matrix(v[:2]).T
+        self.speed = np.matrix(v).T
 
 def coords_input_callback(data):
     z = map(float, data.data.split())
-    X = k_filter.iterate(z[:2])
-    pub.publish(' '.join(map(str, [X[0,0], X[2,0], z[2]])))
+    X = k_filter.iterate(z)
+    pub.publish(' '.join(map(str, [X[0,0], X[2,0], X[4,0]])))
 
 def speed_input_callback(data):
     v = map(float, data.data.split())
