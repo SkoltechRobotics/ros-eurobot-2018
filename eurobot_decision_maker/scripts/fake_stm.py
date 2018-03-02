97b78e5098cd3d107abf8da9ff1bea3c7bfe0127
Mikhail Kurenkov <kurenkov1995@mail.ru>
2018-03-02 19:29:43 +0300

    changed timer in fake_stm

--- eurobot_decision_maker/scripts/fake_stm.py
+++ eurobot_decision_maker/scripts/fake_stm.py
@@ -16,11 +16,12 @@ def fake_responser(time):
     def fake_response(msg):
         rospy.loginfo(msg.data)
         action_name = re.match(r"(\S*)\s[\s\S]*", msg.data).group(1)
-        rospy.sleep(time)
         response_message = action_name + ' ' + 'finished'
         global pub
-        pub.publish(response_message)
-        rospy.loginfo("wrote " + response_message)
+        def cb(e):
+            pub.publish(response_message)
+            rospy.loginfo("wrote " + response_message)
+        rospy.Timer(rospy.Duration(time), cb, oneshot=True)
 
     return fake_response
 
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     rospy.init_node('fake_stm', anonymous=True)
 
     pub = rospy.Publisher('/main_robot/response', String, queue_size=10)
-    sub = rospy.Subscriber('/main_robot/stm_command', String, fake_responser(2.5))
+    sub = rospy.Subscriber('/main_robot/stm_command', String, fake_responser(4))
     rospy.loginfo('Started')
 
     rospy.spin()
