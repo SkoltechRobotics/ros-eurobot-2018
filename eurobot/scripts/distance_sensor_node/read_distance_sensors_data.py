390df1acbdf681d8c80340fb8d310ecd0547f7c4
Mikhail Kurenkov <kurenkov1995@mail.ru>
2018-03-05 12:10:42 +0300

    To window sensor

--- eurobot/scripts/distance_sensor_node/read_distance_sensors_data.py
+++ eurobot/scripts/distance_sensor_node/read_distance_sensors_data.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
         rospy.loginfo("Connect to /dev/ttyACM0 for rangefinder data successfully")
         a = 0.8
         rospy.loginfo("Smooth value for exponential filter is " + str(a))
-        sensors_smooth = np.array([255, 255, 255, 255, 255])
+        sensors_smooth = np.ones((10, 5)) * 255
+        window = np.ones(10) / 10
         while not rospy.is_shutdown():
             s = ser.readline()
             try:
@@ -25,7 +26,9 @@ if __name__ == '__main__':
             else:
                 if sensors_raw.shape[0] == 5:
                     pub_raw.publish(Float32MultiArray(data=sensors_raw))
-                    sensors_smooth = a * sensors_smooth + (1 - a) * sensors_raw
-                    pub_smooth.publish(Float32MultiArray(data=sensors_smooth))
+                    np.roll(sensors_smooth, -1, axis=0)
+                    sensors_smooth[10] = sensors_raw
+                    smooth_data = (window[np.newaxis, :].dot(sensors_smooth))[0]
+                    pub_smooth.publish(Float32MultiArray(data=smooth_dat))
     except rospy.ROSInterruptException:
         pass
