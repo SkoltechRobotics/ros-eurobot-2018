6f4a8d41f394af80454d615ba63a1e3d1dd4952b
Viktor Liviniuk <viktor.liviniuk@gmail.com>
2018-04-14 03:36:38 +0300

    add changes in scripts

--- scripts/main_robot.sh
+++ scripts/main_robot.sh
@@ -1,5 +1,6 @@
-source /opt/ros/kinetic/setup.bash
-source ~/catkin_ws/devel/setup.bash
+#! /bin/bash
+# source /opt/ros/kinetic/setup.bash
+# source ~/catkin_ws/devel/setup.bash
 # export ROS_MASTER_URI=http://192.168.2.104:11311
-export ROS_IP=192.168.88.250
+# export ROS_IP=192.168.88.250
 roslaunch eurobot main_robot.launch
