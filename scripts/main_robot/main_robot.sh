c7f0b9a98255fcc87c335df714587339d2533c7e
Viktor Liviniuk <viktor.liviniuk@gmail.com>
2018-04-16 20:41:19 +0300

    Changes in scripts

--- scripts/main_robot/main_robot.sh
+++ scripts/main_robot/main_robot.sh
@@ -2,9 +2,12 @@
 
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.88.251:11311
+export ROS_MASTER_URI=http://192.168.88.252:11311
 export ROS_IP=192.168.88.250
-dt=$(date '+%d/%m/%Y %H:%M:%S');
+dt=`date -Iseconds`
+sleep 14s
+echo "start script"
 while true; do
-    roslaunch eurobot main_robot.launch > /home/pi/log/main_robot$dt.log
+    echo "try launch"
+    roslaunch eurobot main_robot.launch >> /home/pi/log/main_robot_${dt}.log >> /home/pi/log/main_robot_error_${dt}.log
 done
