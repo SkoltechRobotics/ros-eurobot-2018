db8435e755bee9a8fb0e976cc809cbd8fb4755fb
Mikhail Kurenkov <kurenkov@1995@mail.ru>
2018-04-16 12:41:30 +0300

    Add date to log file

--- scripts/main_robot/main_robot.sh
+++ scripts/main_robot/main_robot.sh
@@ -4,7 +4,7 @@ source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.88.251:11311
 export ROS_IP=192.168.88.250
-
+dt=$(date '+%d/%m/%Y %H:%M:%S');
 while true; do
-    roslaunch eurobot main_robot.launch > /home/pi/log/main_robot.log
+    roslaunch eurobot main_robot.launch > /home/pi/log/main_robot$dt.log
 done
