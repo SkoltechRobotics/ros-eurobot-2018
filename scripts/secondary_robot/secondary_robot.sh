1d12966f3ad67e455fa5727c7c5ce92d79fa9006
Mikhail Kurenkov <kurenkov@1995@mail.ru>
2018-04-16 21:47:41 +0300

    some changes in autorun scirpts

--- scripts/secondary_robot/secondary_robot.sh
+++ scripts/secondary_robot/secondary_robot.sh
@@ -5,9 +5,8 @@ source ~/catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.88.252:11311
 export ROS_IP=192.168.88.250
 dt=`date -Iseconds`
-sleep 1s
-echo "start secondary robot script"
+echo "Start secondary robot script"
 while true; do
-    echo "try secondary robot launch"
+    echo "Try secondary robot launch"
     roslaunch eurobot secondary_robot.launch >> /home/pi/log/secondary_robot_${dt}.log >> /home/pi/log/secondary_robot_error_${dt}.log
 done
