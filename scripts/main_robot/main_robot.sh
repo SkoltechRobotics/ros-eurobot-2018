4c475c1cb6c390d581d624bdf3f6a99a5954b7f5
liviniuk <viktor.liviniuk@gmail.com>
2018-04-17 21:41:35 +0300

    different IP of CTD

--- scripts/main_robot/main_robot.sh
+++ scripts/main_robot/main_robot.sh
@@ -2,11 +2,12 @@
 
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.88.251:11311
+export ROS_MASTER_URI=http://192.168.88.245:11311
 export ROS_IP=192.168.88.250
 dt=`date -Iseconds`
 echo "Start main robot script"
 while true; do
     echo "Try main robot launch"
     roslaunch eurobot main_robot.launch >> /home/pi/log/main_robot_${dt}.log >> /home/pi/log/main_robot_error_${dt}.log
+    sleep 1s
 done
