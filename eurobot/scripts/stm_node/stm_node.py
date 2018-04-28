c2a29f597645c22ede4f9c6d78a1370fc8bf34d8
Mikhail Kurenkov <kurenkov1995@mail.ru>
2018-04-28 11:11:13 +0000

    Comment servo

--- eurobot/scripts/stm_node/stm_node.py
+++ eurobot/scripts/stm_node/stm_node.py
@@ -10,7 +10,7 @@ import tf
 import numpy as np
 
 # libs for servo-motor (home automation panel switch manipulator)
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 
 RATE = 20
@@ -88,10 +88,10 @@ class stm_node(STMprotocol):
         self.wire_timer = rospy.Timer(rospy.Duration(1. / 30), self.wire_timer_callback)
 
         # servo
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.OUT)
-        self.pwm = GPIO.PWM(18, 100)
+        #GPIO.setwarnings(False)
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(18, GPIO.OUT)
+        #self.pwm = GPIO.PWM(18, 100)
 
     def set_twist(self, twist):
         self.send("set_speed", 8, [twist.linear.x, twist.linear.y, twist.angular.z])
@@ -123,19 +123,19 @@ class stm_node(STMprotocol):
         action_name, action_type, args = self.parse_data(data)
 
         # servo
-        if action_type == 256:
-            self.pwm.start(10) # servo manipulator-ON angle
-            self.pwm.ChangeDutyCycle(5)
-
-            def servo_response_wait_stop(event):
-                self.pub_response.publish(action_name + str(" finished"))
-                rospy.sleep(2)
-                self.pwm.ChangeDutyCycle(10)
-                rospy.sleep(1)
-                self.pwm.stop()
-
-            rospy.Timer(rospy.Duration(.2), servo_response_wait_stop, oneshot=True)
-            return
+        # if action_type == 256:
+        #    self.pwm.start(10) # servo manipulator-ON angle
+        #    self.pwm.ChangeDutyCycle(5)
+	#
+        #    def servo_response_wait_stop(event):
+        #        self.pub_response.publish(action_name + str(" finished"))
+        #        rospy.sleep(2)
+        #        self.pwm.ChangeDutyCycle(10)
+        #        rospy.sleep(1)
+        #        self.pwm.stop()
+	#
+        #    rospy.Timer(rospy.Duration(.2), servo_response_wait_stop, oneshot=True)
+        #    return
         
         self.time_started[action_name] = (rospy.get_time(), False)
         rospy.loginfo(self.time_started)
